7908c215d07777df677d3e44f3bcbc5946998ed1
KTH-INHA-16 <kim1120318@naver.com>
2020-09-26 14:50:20 +0900

    6

--- config/settings.py
+++ config/settings.py
@@ -92,12 +92,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            "HOST": "chanllenge.c9vz1y8ht6no.ap-northeast-2.rds.amazonaws.com",
+            'NAME': "chanllenge",
+            'PASSWORD': "kim1120318!",
+            'PORT': "5432",
+            'USER': "postgresql"
+        }
+    }    
 
 
 # Password validation
